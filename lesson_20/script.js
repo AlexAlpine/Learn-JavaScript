96198cc9088dc628ebb4c88fc05436bfdc16cefc
AlexAlpine <alex6122223@gmail.com>
2024-08-28 18:17:18 +0200

    lesson_20

--- lesson_20/script.js
+++ lesson_20/script.js
@@ -1,30 +1,42 @@
-// Запрос к первому API для получения данных о широте, долготе и городе:
-
-fetch(
-  "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true"
-)
-  .then((response) => response.json())
-  .then((data) => {
-    const { latitude, longitude, name } = data.current_weather;
-    console.log(
-      `Latitude: ${latitude}, Longitude: ${longitude}, City: ${name}`
-    );
-  })
-  
-
-fetch(
-  `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`
-)
-  .then((response) => response.json())
-  .then((data) => {
-    const { temperature, windspeed, weathercode } = data.current_weather;
-    console.log(
-      `Temperature: ${temperature}°C, Wind speed: ${windspeed}m/s, Weather code: ${weathercode}`
-    );
-  });
- 
-  fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`)
-  .then(response => response.json())
+// запрос к первой api
+fetch("https://get.geojs.io/v1/ip/geo.json")
+  .then(res => res.json())
   .then(data => {
-    const { temperature, windspeed, weathercode } = data.current_weather;
-    console.log(`Temperature: ${temperature}°C, Wind speed: ${windspeed}m/s, Weather code: ${weathercode}`);})
+    // деструктуризация данных из data
+    const { city, latitude, longitude } = data;
+    // запрос ко второй api с данными и первой
+    fetch(
+      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`
+    )
+      .then(res => res.json())
+      .then(data => {
+        // создаем карточку и ее отдельные элементы
+        const card = document.createElement('div')
+        card.className = 'weather-card'
+
+        const h3 = document.createElement('h3')
+        h3.textContent = city
+
+        const p = document.createElement('p')
+        p.textContent = `Temperature: ${data.current_weather.temperature} ${data.current_weather_units.temperature}`
+
+        const p1 = document.createElement('p')
+        p1.textContent = `Wind: ${data.current_weather.interval} ${data.current_weather_units.interval}`
+
+        const p2 = document.createElement('p')
+        p2.textContent = `Weather: ${decodeWeather(data.current_weather.weathercode)}`
+        // добавляем элементы к карточке
+        card.append(h3, p, p1, p2)
+        // добавляем карточку на страницу
+        document.body.appendChild(card)
+      });
+  });
+
+
+  function decodeWeather(code) {
+    switch (code) {
+      case 0:
+        return 'Clear sky'
+    }
+    // ! здесь должны быть описаны все случаи в  case чтобы приложение работало корректно
+  }
